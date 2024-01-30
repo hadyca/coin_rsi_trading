4cec2b99c5ab8325e6823badb00bf2f603764e44
Jae Hyung Kim <SC425@ypsa.com>
2024-01-30 15:51:30 +0600

    업비트 기본정보fetch 완료

---
+++ utils/upbitAuth.js
@@ -0,0 +1,43 @@
+import { v4 } from "uuid";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import querystring from "querystring";
+
+const access_key = process.env.UPBIT_OPEN_API_ACCESS_KEY;
+const secret_key = process.env.UPBIT_OPEN_API_SECRET_KEY;
+
+export function up_getAuth_noParams() {
+  const payload = {
+    access_key: access_key,
+    nonce: v4(),
+  };
+  const token = jwt.sign(payload, secret_key);
+  const auth = {
+    headers: { Authorization: `Bearer ${token}` },
+    cache: "no-store",
+  };
+  return auth;
+}
+
+export function up_getAuth_params(params) {
+  const query = querystring.stringify(params);
+
+  const hash = crypto.createHash("sha512");
+  const queryHash = hash.update(query, "utf-8").digest("hex");
+
+  const payload = {
+    access_key: access_key,
+    nonce: v4(),
+    query_hash: queryHash,
+    query_hash_alg: "SHA512",
+  };
+
+  const token = jwt.sign(payload, secret_key);
+
+  const auth = {
+    headers: { Authorization: `Bearer ${token}` },
+    cache: "no-store",
+  };
+
+  return auth;
+}
