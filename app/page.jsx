be1a94d74c83a04af2405839e631ed0dd53b6567
unknown <hadycas@gmail.com>
2024-02-02 23:43:08 +0900

    시장가 매수 100%현금 에러..

--- app/page.jsx
+++ app/page.jsx
@@ -2,5 +2,6 @@ import { app } from "@/lib/app";
 
 export default async function Page() {
   await app();
+
   return <div>시작페이지</div>;
 }
---
+++ app/user/page.jsx
@@ -0,0 +1,6 @@
+import test from "@/lib/test";
+
+export default function Page() {
+  test();
+  return <div>유저 페이지2222</div>;
+}
